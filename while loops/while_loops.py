811abb53ee4ce97a078bcfa9b8d90f66c33575f6
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-09-27 18:05:57 -0400

    logics written

--- while loops/while_loops.py
+++ while loops/while_loops.py
@@ -1,13 +1,37 @@
-# Start with the number 1
-number = 1
+# # Start with the number 1
+# number = 1
 
-# Keep looping as long as the number is less than or equal to 7
-while number <= 7:
+# # Keep looping as long as the number is less than or equal to 7
+# while number <= 7:
 
-    # Display the number
-    print (f"The number is: {number}")
+#     # Display the number
+#     print (f"The number is: {number}")
 
-# Update the number to be 1 more than it was
-    number = number + 1
+# # Update the number to be 1 more than it was
+#     number = number + 1
 
-print("This is the end of the loop.")
+# print("This is the end of the loop.")
+
+
+
+
+# Start with the number 0
+# number = ""
+
+# question = int(input("Please type a positive number: "))
+# while question <= -1:
+#     print("Sorry, that is a negative number. Please try again.")
+#     question = int(input("Please type a positive number: "))
+# print(f"The number is: {question}")
+
+
+
+# Start with the variable
+response = ""
+
+question_1 = input("May I have a piece of candy? ")
+main_question = question_1.upper()
+while response == "no":
+    input("May I have a piece of candy? ")
+print("Thank You.")
+
