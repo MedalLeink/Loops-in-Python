921b4b3450f033bc58aef2f58e2cb01298e59c4c
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-09-27 13:33:34 -0400

    While loops

---
+++ while loops/while_loops.py
@@ -0,0 +1,13 @@
+# Start with the number 1
+number = 1
+
+# Keep looping as long as the number is less than or equal to 7
+while number <= 7:
+
+    # Display the number
+    print (f"The number is: {number}")
+
+# Update the number to be 1 more than it was
+    number = number + 1
+
+print("This is the end of the loop.")
